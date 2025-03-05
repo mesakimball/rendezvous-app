54773abaca2c88db9dec288bb97b3e1c86c615b7
Brandon Kent <10407384+brandonk3nt@users.noreply.github.com>
2025-03-04 21:36:17 -0700

    hero (#1)

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,15 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
-
 export default function Home() {
-  const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
+    <div class="flex flex-col items-center justify-center h-screen">
+      <video
+        loop
+        autoPlay
+        muted
+        class="h-screen w-screen object-cover fixed top-0 left-0 -z-1"
+      >
+        <source src="/hero.mp4" type="video/mp4" />
+      </video>
+      <img src="/logo-white.png" class="absolute z-10 p-4" />
     </div>
   );
 }
