574f717021e0841a466890b91ba65b45100e1e26
Brandon Kent <brandonk3nt@gmail.com>
2025-03-06 16:16:48 -0700

    timer updates

--- components/UpcomingDates.tsx
+++ components/UpcomingDates.tsx
@@ -1,7 +1,7 @@
 export default function UpcomingDates() {
   return (
     <div class="py-32 px-8 md:p-32">
-      <h1 class="text-center pb-8 text-3xl">Upcoming Dates</h1>
+      <h1 class="text-center font-bold pb-8 text-3xl">Upcoming Dates</h1>
       <div>
         <h3 class="font-bold">March 19</h3>
         <p class="italic">Mini rendezvous activity</p>
--- islands/CountDown.tsx
+++ islands/CountDown.tsx
@@ -41,26 +41,37 @@ export default function CountDown({ endDate, endLabel }: CountDownProps) {
   }, []);
 
   return (
-    <div class="flex flex-col justify-center py-32 px-8 md:p-32 bg-lime-950 text-white">
-      <h1 class="text-center pb-8 text-3xl">{endLabel}</h1>
+    <div class="flex flex-col items-center justify-center py-32 px-8 md:p-32 bg-lime-950 text-white">
+      <h1 class="text-center font-bold pb-8 text-3xl">{endLabel}</h1>
       <p class="italic text-center pb-8">
         Stake registration for Rendezvous coming soon!
       </p>
-      <div class="flex flex-row items-center justify-around text-xl">
-        <div class="flex flex-col items-center justify-center">
-          <span class="font-bold">{("0" + t.value.days).slice(-2)}</span>
+      <div class="flex flex-row items-center justify-around text-xl max-w-96">
+        <div class="flex flex-col items-center justify-center px-6">
+          <span class="font-bold text-2xl">
+            {("0" + t.value.days).slice(-2)}
+          </span>
           <span>days</span>
         </div>
-        <div class="flex flex-col items-center justify-center">
-          <span class="font-bold">{("0" + t.value.hours).slice(-2)}</span>
+        <span class="text-bold text-3xl">:</span>
+        <div class="flex flex-col items-center justify-center px-6">
+          <span class="font-bold text-2xl">
+            {("0" + t.value.hours).slice(-2)}
+          </span>
           <span>hrs</span>
         </div>
-        <div class="flex flex-col items-center justify-center">
-          <span class="font-bold">{("0" + t.value.minutes).slice(-2)}</span>
+        <span class="text-bold text-3xl">:</span>
+        <div class="flex flex-col items-center justify-center px-6">
+          <span class="font-bold text-2xl">
+            {("0" + t.value.minutes).slice(-2)}
+          </span>
           <span>min</span>
         </div>
-        <div class="flex flex-col items-center justify-center">
-          <span class="font-bold">{("0" + t.value.seconds).slice(-2)}</span>
+        <span class="text-bold text-3xl">:</span>
+        <div class="flex flex-col items-center justify-center px-6">
+          <span class="font-bold text-2xl">
+            {("0" + t.value.seconds).slice(-2)}
+          </span>
           <span>sec</span>
         </div>
       </div>
