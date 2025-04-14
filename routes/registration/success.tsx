9a8220b37ccb0e6f35d8143279df2b8f69a653a5
brandonk3nt <brandonk3nt@gmail.com>
2025-04-14 15:18:00 -0700

    added secret to deploy

--- routes/registration/success.tsx
+++ routes/registration/success.tsx
@@ -9,9 +9,15 @@ export default function RegisterSuccessPage() {
             Thank you for registering!
           </h1>
           <p class="mb-8 text-lg font-normal lg:text-xl sm:px-16 xl:px-48 text-gray-300">
-            We will calculate totals based on registration count and then reach
-            out to your ward clerk with next steps.
+            We’ll process all registrations and notify your ward clerk with
+            further instructions once totals are finalized.
           </p>
+          <a
+            href="/"
+            class="inline-flex items-center justify-center px-4 py-2.5 text-base font-medium text-center text-white bg-sky-400 rounded-lg hover:bg-sky-500 focus:ring-4 focus:ring-sky-600"
+          >
+            Back To Home
+          </a>
         </div>
       </section>
       <FooterSection />
