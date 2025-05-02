aad6bf0a3bae29584f49cfb68cf2085056ed542c
brandonk3nt <brandonk3nt@gmail.com>
2025-05-01 19:08:20 -0700

    redirect for login page

--- components/RegisterSection.tsx
+++ components/RegisterSection.tsx
@@ -16,7 +16,7 @@ export default function RegisterSection() {
           </p>
           <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
             <a
-              href="/registration"
+              href="/registration/temp"
               class="inline-flex items-center justify-center px-4 py-2.5 text-base font-medium text-center text-white bg-sky-400 rounded-lg hover:bg-sky-500 focus:ring-4 focus:ring-sky-600"
             >
               Register
